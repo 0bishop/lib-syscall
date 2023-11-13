8eb2825a9560a97514625858910b420cfdd370de
bishop <87420893+0bishop@users.noreply.github.com>
2023-11-13 17:33:22 +0100

    fix(optimise) : optimise code

--- include/syscall.h
+++ include/syscall.h
@@ -14,25 +14,26 @@
         __builtin_va_start(args, syscall_number);
 
         uint64_t ret;
-        asm volatile("mov %1, %%rax\n"
-                    "mov %2, %%rdi\n"
-                    "mov %3, %%rsi\n"
-                    "mov %4, %%rdx\n"
-                    "mov %5, %%r10\n"
-                    "mov %6, %%r8\n"
-                    "mov %7, %%r9\n"
-                    "syscall\n"
-                    "mov %%rax, %0\n"
-                    : "=r"(ret)
-                    : "r"(syscall_number),
-                    "r"(__builtin_va_arg(args, uint64_t)),
-                    "r"(__builtin_va_arg(args, uint64_t)),
-                    "r"(__builtin_va_arg(args, uint64_t)),
-                    "r"(__builtin_va_arg(args, uint64_t)),
-                    "r"(__builtin_va_arg(args, uint64_t)),
-                    "r"(__builtin_va_arg(args, uint64_t))
-                    : "rax", "rdi", "rsi", "rdx", "r10", "r8", "r9");
-
+        __asm__ __volatile__(
+            "mov %1, %%rax\n"
+            "mov %2, %%rdi\n"
+            "mov %3, %%rsi\n"
+            "mov %4, %%rdx\n"
+            "mov %5, %%r10\n"
+            "mov %6, %%r8\n"
+            "mov %7, %%r9\n"
+            "syscall\n"
+            "mov %%rax, %0\n"
+            : "=r"(ret)
+            : "r"(syscall_number),
+            "r"(__builtin_va_arg(args, uint64_t) ?: 0),
+            "r"(__builtin_va_arg(args, uint64_t) ?: 0),
+            "r"(__builtin_va_arg(args, uint64_t) ?: 0),
+            "r"(__builtin_va_arg(args, uint64_t) ?: 0),
+            "r"(__builtin_va_arg(args, uint64_t) ?: 0),
+            "r"(__builtin_va_arg(args, uint64_t) ?: 0)
+            : "rax", "rdi", "rsi", "rdx", "r10", "r8", "r9"
+        );
         __builtin_va_end(args);
 
         return ret;
